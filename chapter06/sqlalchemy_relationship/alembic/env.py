2bb7eb6096b0dc6b6d5325ae5715a31fed2e5950
alexeylitovchenko <algaritmno822@gmail.com>
2023-10-29 23:13:18 +0300

    qwert

--- chapter06/sqlalchemy_relationship/alembic/env.py
+++ chapter06/sqlalchemy_relationship/alembic/env.py
@@ -1,9 +1,9 @@
+import typing
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
 from chapter06.sqlalchemy_relationship.models import Base
 
 # this is the Alembic Config object, which provides
@@ -59,15 +59,15 @@ def run_migrations_online() -> None:
 
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        typing.cast(
+            dict[str, typing.Any], config.get_section(config.config_ini_section)
+        ),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
